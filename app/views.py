34a30fa721f3d56d61cc22e45b19bd0667be515c
9pedobear <9pedobear@gmail.com>
2022-06-08 10:53:09 +0600

    02 upload 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
 from django.views.generic import TemplateView
+from .models import Book
+from .forms import BookForm
 
 
 class Home(TemplateView):
@@ -14,6 +16,23 @@ def upload(request):
         print(uploaded_file.size)
         fs = FileSystemStorage()
         print(fs)
-        fs.save(uploaded_file.name, uploaded_file)
-        # context['url'] = fs.url(name)
-    return render(request, 'upload.html')
+        name = fs.save(uploaded_file.name, uploaded_file)
+        print(name)
+        context['url'] = fs.url(name)
+    return render(request, 'upload.html', context)
+
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html',{'books': books})
+
+def upload_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'upload_book.html',{'form': form})
