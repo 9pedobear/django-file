17d33445f328db65ceb944c2221b46c047e7c138
9pedobear <9pedobear@gmail.com>
2022-06-08 08:43:38 +0600

    02 upload

--- app/views.py
+++ app/views.py
@@ -7,10 +7,12 @@ class Home(TemplateView):
     template_name = 'home.html'
 
 def upload(request):
-    # context = {}
-    # if request.method == 'POST':
-    #     uploaded_file = request.FILES['document']
-    #     fs = FileSystemStorage()
-    #     name = fs.save(uploaded_file.name, uploaded_file)
-    #     context['url'] = fs.url(name)
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
+        # fs = FileSystemStorage()
+        # name = fs.save(uploaded_file.name, uploaded_file)
+        # context['url'] = fs.url(name)
     return render(request, 'upload.html')
